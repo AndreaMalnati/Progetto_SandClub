6cf33554bc33d9760d852015d94923ca4e3af6ef
Davide-Domenico-Montani <d.montani1@campus.unimib.it>
2023-12-10 18:35:07 +0100

    Aggiunti bottoni e barre in activity_main.xml

--- app/src/main/java/sandclub/beeradvisor/MainActivity.java
+++ app/src/main/java/sandclub/beeradvisor/MainActivity.java
@@ -3,12 +3,25 @@ package sandclub.beeradvisor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageButton home;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        home = findViewById(R.id.button1);
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Cliccato", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
     }
 }
