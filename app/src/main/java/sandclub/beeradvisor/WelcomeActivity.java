b05277b64787cc4bbd094ccce770cf1d207b9482
Davide-Domenico-Montani <d.montani1@campus.unimib.it>
2023-12-15 23:14:39 +0100

    Risolto registrazione con google e scrittura su database

--- app/src/main/java/sandclub/beeradvisor/WelcomeActivity.java
+++ app/src/main/java/sandclub/beeradvisor/WelcomeActivity.java
@@ -38,7 +38,7 @@ public class WelcomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
-        Naviga
+
 
     }
 
--- app/src/main/java/sandclub/beeradvisor/ui/welcome/AuthenticationFragment.java
+++ app/src/main/java/sandclub/beeradvisor/ui/welcome/AuthenticationFragment.java
@@ -29,6 +29,7 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.HashMap;
@@ -38,18 +39,21 @@ import sandclub.beeradvisor.MainActivity;
 import sandclub.beeradvisor.R;
 import sandclub.beeradvisor.RegisterActivity;
 import sandclub.beeradvisor.WelcomeActivity;
+import sandclub.beeradvisor.model.User;
 
 public class AuthenticationFragment extends Fragment {
 
     Button login, register;
 
-    ImageButton google;
+    Button google;
     FirebaseAuth auth;
     FirebaseDatabase database;
+    private Task<Void> mDatabase;
     GoogleSignInClient googleSignInClient;
     int RC_SIGN_IN = 20;
 
-
+    //DA CAMBIARE E METTERE NEI CONST QUANDO SI MERGERA'
+    String databaseUrl = "https://progetto-sandclub-default-rtdb.europe-west1.firebasedatabase.app/";
 
     public AuthenticationFragment() {
         // Required empty public constructor
@@ -84,6 +88,7 @@ public class AuthenticationFragment extends Fragment {
         database = FirebaseDatabase.getInstance();
 
 
+
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -106,7 +111,7 @@ public class AuthenticationFragment extends Fragment {
                 .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
-        googleSignInClient = GoogleSignIn.getClient(this, gso);
+        googleSignInClient = GoogleSignIn.getClient(requireActivity(), gso);
 
         google.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -118,40 +123,33 @@ public class AuthenticationFragment extends Fragment {
 
     private void googleSignIn(){
         Intent intent = googleSignInClient.getSignInIntent();
-        //startActivityForResult(intent, RC_SIGN_IN);
-        signInLauncher.launch(intent);
+        startActivityForResult(intent, RC_SIGN_IN);
+        //signInLauncher.launch(intent);
+
     }
 
-    /*@Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(requestCode == RC_SIGN_IN){
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            try{
-                GoogleSignInAccount account = task.getResult(ApiException.class);
-                firebaseAuth(account.getIdToken());
-
-
-            }catch(Exception e){
-                Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == getActivity().RESULT_OK) {
+                Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+                try {
+                    GoogleSignInAccount account = task.getResult(ApiException.class);
+                    firebaseAuth(account.getIdToken());
+                } catch (ApiException e) {
+                    Toast.makeText(requireActivity(), "ApiException: " + e.getStatusCode(), Toast.LENGTH_SHORT).show();
+                } catch (Exception e) {
+                    Toast.makeText(requireActivity(), "Exception: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                // Handle the case where the sign-in was not successful
+                Toast.makeText(requireActivity(), "Sign-in failed", Toast.LENGTH_SHORT).show();
             }
         }
-    }*/
-    // Dichiarazione del launcher
-    private final ActivityResultLauncher<Intent> signInLauncher =
-            registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
-                    result -> {
-                        if (result.getResultCode() == RC_SIGN_IN) {
-                            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-                            try {
-                                GoogleSignInAccount account = task.getResult(ApiException.class);
-                                firebaseAuth(account.getIdToken());
-                            } catch (Exception e) {
-                                Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    });
+    }
+
 
     private void firebaseAuth(String idToken) {
         AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
@@ -161,17 +159,12 @@ public class AuthenticationFragment extends Fragment {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()){
                             FirebaseUser user = auth.getCurrentUser();
-                            HashMap<String, Object> map = new HashMap<>();
-                            map.put("id", user.getUid());
-                            map.put("name", user.getDisplayName());
-                            map.put("profile", user.getPhotoUrl().toString());
-
-                            database.getReference().child("users").child(user.getUid()).setValue(map);
-
-                            Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+                            User newUser = new User(user.getUid(), user.getDisplayName(), user.getDisplayName(), user.getEmail(), "", user.getPhotoUrl().toString());
+                            mDatabase = FirebaseDatabase.getInstance(databaseUrl).getReference().child("user").child(user.getUid()).setValue(newUser);;
+                            Intent intent = new Intent(getContext(), MainActivity.class);
                             startActivity(intent);
                         }else{
-                            Toast.makeText(WelcomeActivity.this, "Qualcosa è andato storto", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(getContext(), "Qualcosa è andato storto", Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
